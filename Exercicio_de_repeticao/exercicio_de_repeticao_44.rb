1c85015226cfb7ccdfce613d40cd4c3677ae8f95
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-13 19:33:07 +1000

    add novos exercicios

---
+++ Exercicio_de_repeticao/exercicio_de_repeticao_44.rb
@@ -0,0 +1,18 @@
+#Em uma eleição presidencial existem quatro candidatos. Os votos são informados
+#por meio de código. Os códigos utilizados são: 1 , 2, 3, 4 - Votos para os 
+#respectivos candidatos (você deve montar a tabela ex: 1 - Jose/ 2- João/etc)
+#5 - Voto Nulo
+#6 - Voto em Branco
+#Faça
+#a. O
+#b. O
+#c. O
+#d. A
+#e. A
+#um programa que calcule e mostre:
+#total de votos para cada candidato;
+#total de votos nulos;
+#total de votos em branco;
+#percentagem de votos nulos sobre o total de votos;
+#percentagem de votos em branco sobre o total de votos.
+#Para finalizar o conjunto de votos tem-se o valor zero.
