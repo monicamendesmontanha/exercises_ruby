18f4c37cffe6d8d9b067a0a8f5aafffb9f27021b
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-15 16:58:14 +1000

    rever pq resposta esta errada

--- Exercicio_com_listas/exercicio_com_listas_04.rb
+++ Exercicio_com_listas/exercicio_com_listas_04.rb
@@ -1,35 +1,29 @@
 #Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes
 #foram lidas. Imprima as consoantes.
 
-vetor = []
+letras = []
 
 print "Digite um nome com 10 letras: "
-letras = gets.chomp
-#letras = gets.split('')
+letras = gets.split('')
 
-vetor << letras
-puts vetor
-
-vogais = []
 consoantes = []
 
-#TENTATIVA COM FOR
-#for n in vetor
-#  if n == "a" || n == "e" || n == "i" || n == "o" || n == "u"
-#    vogais << n
-#  else n != "a" && n != "e" && n != "i" && n != "o" && n != "u"
+#1) FOR
+for n in letras
+  if n != "a" && n != "e" && n != "i" && n != "o" && n != "u"
+    consoantes << n
+  end
+end
+
+#2) EACH
+#letras.each do |n|
+#  if n != "a" && n != "e" && n != "i" && n != "o" && n != "u"
 #    consoantes << n
 #  end
 #end
 
-# TENTATIVA COM EACH
-#vetor.each do |n|
-#  consoantes << n == "a" || n == "e" || n == "i" || n == "o" || n == "u"
-#end
+#3) SELECT
+#consoantes = letras.select { |n| n != "a" && n != "e" && n != "i" && n != "o" && n != "u"}
 
-# TENTATIVA COM SELECT
-#consoantes << vetor.select { |n| n == "a" || n == "e" || n == "i" || n == "o" || n == "u"}
-
-#puts vogais
 puts consoantes
-puts "O vetor é composto por #{consoantes.size} consoantes."
+puts "Quantidade de consoantes no nome: #{consoantes.size}."
