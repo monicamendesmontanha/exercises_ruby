ab4b10fb18246ec4308ba203af2f882b6dc8c13e
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-15 17:35:13 +1000

    Exercício resolvido (número primo tipo II)

--- Exercicio_de_repeticao/exercicio_de_repeticao_35.rb
+++ Exercicio_de_repeticao/exercicio_de_repeticao_35.rb
@@ -1,2 +1,13 @@
 #Encontrar números primos é uma tarefa difícil. Faça um programa que gera uma
 #lista dos números primos existentes entre 1 e um número inteiro informado pelo usuário.
+
+print "Quer contar até que número? "
+limite = gets.to_i
+
+contador = 0
+while contador < limite
+  contador = contador + 1
+  if contador %2 != 0
+    puts contador
+  end
+end
