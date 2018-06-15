e3aa4572524d5f686fdf0b93c1bdb8bb73cf95f5
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-15 17:26:24 +1000

    Exercício resolvido (número primo)

--- Exercicio_de_repeticao/exercicio_de_repeticao_34.rb
+++ Exercicio_de_repeticao/exercicio_de_repeticao_34.rb
@@ -2,3 +2,9 @@
 #Criptografia. Um número primo é aquele que é divisível apenas por um e por ele mesmo. 
 #Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
 
+print "Digite um número: "
+numero = gets.to_i
+
+if numero % 2 != 0 
+  puts "O número #{numero} é primo!"
+end
