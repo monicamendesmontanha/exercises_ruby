35f79aac0917f9b7d1109198aba116217a62d415
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-18 17:26:30 +1000

    Solução Incompleta (Problema com o Float)

--- Exercicio_de_repeticao/exercicio_de_repeticao_38.rb
+++ Exercicio_de_repeticao/exercicio_de_repeticao_38.rb
@@ -4,3 +4,22 @@
 #c. A partir de 1997 (inclusive), os aumentos salariais sempre correspondem ao dobro do percentual do ano anterior.
 #Faça um programa que determine o salário atual desse funcionário.
 #Após concluir isto, altere o programa permitindo que o usuário digite o salário inicial do funcionário.
+
+CONTRATACAO = 1995
+SALARIO_INICIAL = 1.000
+
+print "Qual ano que vocẽ quer saber o valor do seu salário? "
+ano = gets.to_f
+
+aumento_anual = 1.5
+while ano > 1997 do
+  novo_aumento_anual = aumento_anual * 2
+  aumento_anual << novo_aumento_anual
+  #return aumento_anual
+  salario_atual = aumento_anual * SALARIO_INICIAL
+  ano = ano + 1
+  return salario_atual
+end
+
+
+puts "R$ #{salario_atual}"
