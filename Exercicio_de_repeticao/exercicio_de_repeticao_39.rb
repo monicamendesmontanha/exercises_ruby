7b320090b561796ebd01f65f7cd6f0130f90e884
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-18 17:40:52 +1000

    Solução Incompleta (Maior e Menor número de dois arrays interligados)

--- Exercicio_de_repeticao/exercicio_de_repeticao_39.rb
+++ Exercicio_de_repeticao/exercicio_de_repeticao_39.rb
@@ -2,3 +2,25 @@
 #representando o número do aluno e o segundo representando a sua altura em
 #centímetros. Encontre o aluno mais alto e o mais baixo. Mostre o número do
 #aluno mais alto e o número do aluno mais baixo, junto com suas alturas.
+
+conjunto_codigo = []
+conjunto_altura = []
+
+
+alunos = 0
+
+while alunos < 3 do
+
+  print "Código do aluno: "
+  codigo = gets.split(',')
+  conjunto_codigo << codigo
+
+  print "Altura do aluno: "
+  altura = gets.split(',')
+  conjunto_altura << altura
+
+  alunos = alunos + 1
+end
+
+puts "#{conjunto_codigo}"
+puts "#{conjunto_altura}"
