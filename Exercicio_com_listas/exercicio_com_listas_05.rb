c2878683a3edd2c11785231e9405e61fd739f779
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-20 13:50:31 +1000

    Exercício resolvido (Arrays pares e ímpares)

--- Exercicio_com_listas/exercicio_com_listas_05.rb
+++ Exercicio_com_listas/exercicio_com_listas_05.rb
@@ -1,3 +1,26 @@
 #Faça um Programa que leia 20 números inteiros e armazene-os num vetor.
 #Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. 
 #Imprima os três vetores.
+
+array = []
+array_par = []
+array_impar = []
+
+contador = 0
+while contador < 10 do
+  print "Digite um numero: "
+  numero = gets.to_i
+  array << numero
+
+  if numero % 2 == 0
+    array_par << numero
+  else
+    array_impar << numero
+  end
+
+  contador = contador + 1
+end
+
+puts "Array: #{array}"
+puts "Pares: #{array_par}"
+puts "Ímpares: #{array_impar}"
