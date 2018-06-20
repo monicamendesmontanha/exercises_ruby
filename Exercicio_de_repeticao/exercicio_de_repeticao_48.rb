f3f5e565a6afaea8a28dd9a8cc19260de079f949
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-20 16:38:27 +1000

    Solução Incompleta (Não sei fazer reversão)

--- Exercicio_de_repeticao/exercicio_de_repeticao_48.rb
+++ Exercicio_de_repeticao/exercicio_de_repeticao_48.rb
@@ -1,3 +1,12 @@
 #Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero invertido.
 #Exemplo: 12376489
 #=> 98467321
+
+array = []
+
+print "Digite uma sequência de 5 números: "
+number = gets
+
+array << number
+
+puts array.reverse!
