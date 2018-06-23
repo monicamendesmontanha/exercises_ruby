e7c5bf04edd9572b150314c14c404c8f4198224a
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-23 20:42:45 +1000

    Exercício Resolvido (Fatorial = exercício 17)

--- Exercicio_de_repeticao/exercicio_de_repeticao_32.rb
+++ Exercicio_de_repeticao/exercicio_de_repeticao_32.rb
@@ -2,3 +2,20 @@
 #usuário. Ex.: 5!=5.4.3.2.1=120. A saída deve ser conforme o exemplo abaixo:
 #Fatorial de: 5
 #5! = 5 . 4 . 3 . 2 . 1 = 120
+
+print "Qual fatorial você quer calular? "
+contador = gets.to_i
+
+#contador = 5
+fator = contador
+fatorial = contador
+array = []
+
+while contador > 1 do
+  fatorial = fatorial * (contador - 1)
+  contador = contador - 1
+  array << contador
+end
+
+print "#{fator}! X #{array} = #{fatorial}"
+#puts fatorial
