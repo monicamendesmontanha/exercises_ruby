8ec9da852694e57af33d45234f4e003b2ebc66e4
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-25 21:35:42 +1000

    Exercício Resolvido (Array dentro de um array)

--- Exercicio_com_listas/exercicio_com_listas_06.rb
+++ Exercicio_com_listas/exercicio_com_listas_06.rb
@@ -1,15 +1,34 @@
 #Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num
 #vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
 
-notas = []
-contador = 0
+# TOTAL_NOTAS.times do
+#   print "Digite a nota: "
+#   notas = gets.to_f
+#   notas << nota
+# end
 
-while contador < 4 do
-  print "Digite a nota: "
-  nota = gets.to_f
-  notas << nota
-  
-  contador = contador + 1
+TOTAL_ALUNOS = 2
+TOTAL_NOTAS = 3
+
+aprovados = []
+
+TOTAL_ALUNOS.times do |n|
+  notas = []
+  TOTAL_NOTAS.times do
+    print "Aluno #{n} :: Digite a nota: "
+    notas << gets.to_f
+  end
+
+  media = notas.sum/notas.size
+
+  if media >= 7
+    aprovados << media
+  end
+
+  print "Notas do aluno #{n}:"
+  puts notas
+  puts "Média: #{media.round(2)}"
+  puts "–––––––––––––––"
 end
 
-puts "Notas: #{notas}"
+puts "O número de alunos com média maior ou igual a 7.0 são: #{aprovados.size}"
