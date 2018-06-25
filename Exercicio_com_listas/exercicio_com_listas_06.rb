e3c63d122d8abbc3adc5fd86d2b3986b48463435
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-25 13:19:47 +1000

    Exercício Incompleto (Armazenar em arrays diferentes e depois juntas)

--- Exercicio_com_listas/exercicio_com_listas_06.rb
+++ Exercicio_com_listas/exercicio_com_listas_06.rb
@@ -1,2 +1,15 @@
 #Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num
 #vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
+
+notas = []
+contador = 0
+
+while contador < 4 do
+  print "Digite a nota: "
+  nota = gets.to_f
+  notas << nota
+  
+  contador = contador + 1
+end
+
+puts "Notas: #{notas}"
