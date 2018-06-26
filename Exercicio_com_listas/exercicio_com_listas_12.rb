9b09b9dd659ed673626c97ac9b6cc8cfa011b632
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-26 16:56:34 +1000

    Adicionado raciocício do que fazer

--- Exercicio_com_listas/exercicio_com_listas_12.rb
+++ Exercicio_com_listas/exercicio_com_listas_12.rb
@@ -1,2 +1,7 @@
 #Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos 
 #com mais de 13 anos possuem altura inferior à média de altura desses alunos.
+
+#1) pedir idades e alturas dos alunos
+#2) fazer a media da altura desses alunos
+#3) fazer um array com todos alunos com mais de 13 anos
+#4) retirar todos alunos com 13 que tem altura abaixo da média geral dos alunos
