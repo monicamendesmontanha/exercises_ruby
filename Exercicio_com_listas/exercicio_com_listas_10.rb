a5738c04dd726fef271f70ee6ba9bdef83a10f5f
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-26 16:50:01 +1000

    Exercício Incompleto (Pegar elementos intercalados de dois vetores)

--- Exercicio_com_listas/exercicio_com_listas_10.rb
+++ Exercicio_com_listas/exercicio_com_listas_10.rb
@@ -1,2 +1,22 @@
 #Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos, 
 #cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
+
+
+QUANTIDADE_DE_ELEMENTOS = 4
+vetor_01 = []
+vetor_02 = []
+
+QUANTIDADE_DE_ELEMENTOS.times do |n|
+  puts "------------------"
+  puts "#{n+1}"
+  print "Digite o primeiro número: "
+  vetor_01 << gets.to_i
+  print "Digite o segundo número: "
+  vetor_02 << gets.to_i
+end
+
+
+puts "#{vetor_01}"
+puts "#{vetor_02}"
+
+#COMO PEGAR ELEMENTOS INTERCALADOS DOS DOIS VETORES E CRIAR UM VETOR MISTO ?
