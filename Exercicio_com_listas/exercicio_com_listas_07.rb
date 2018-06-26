308718db14169c4a742fb5a4da0d4453db3445e4
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-26 11:07:35 +1000

    Exercício Incompleto (Falta a "multiplicação dos Nºs do array)

--- Exercicio_com_listas/exercicio_com_listas_07.rb
+++ Exercicio_com_listas/exercicio_com_listas_07.rb
@@ -1,1 +1,19 @@
 #Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
+
+TOTAL_DE_NUMEROS = 5
+numeros = []
+
+TOTAL_DE_NUMEROS.times do
+  print "Digite um número: "
+  numeros << gets.to_i
+end
+
+puts "Números: #{numeros}"
+puts "Soma dos números: #{numeros.sum}"
+
+total = 0
+numeros.each do |n|
+  total = total * n
+end
+
+puts "Multiplicação dos números: #{total}"
