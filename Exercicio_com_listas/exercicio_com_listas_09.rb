69964296344b614603ad1836bdb6cde04a07b6d3
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-26 16:40:43 +1000

    Exercício Resolvido (Elevar elementos de um array ao quadrado)

--- Exercicio_com_listas/exercicio_com_listas_09.rb
+++ Exercicio_com_listas/exercicio_com_listas_09.rb
@@ -1,2 +1,13 @@
 #Faça um Programa que leia um vetor A com 10 números inteiros, 
 #calcule e mostre a soma dos quadrados dos elementos do vetor.
+
+TOTAL_DE_NUMEROS = 10
+vetor = []
+
+TOTAL_DE_NUMEROS.times do
+print "Digite um número: "
+vetor << gets.to_i
+end
+
+vetor.map! { |a| a**2 }
+puts "#{vetor.sum}"
