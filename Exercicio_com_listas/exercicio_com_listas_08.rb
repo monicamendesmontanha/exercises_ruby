677780b06c5db749f93f5008a7b936e9e9474793
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-06-26 12:25:39 +1000

    Exercício Completo (Idades e Alturas em Ordem Inversa)

--- Exercicio_com_listas/exercicio_com_listas_08.rb
+++ Exercicio_com_listas/exercicio_com_listas_08.rb
@@ -1,2 +1,16 @@
 #Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu 
 #respectivo vetor. Imprima a idade e a altura na ordem inversa a ordem lida.
+
+TOTAL_DE_PESSOAS = 5
+idades = []
+alturas = []
+
+TOTAL_DE_PESSOAS.times do |n|
+  print "#{n+1} - Digite sua idade: "
+  idades << gets.to_f
+  print "#{n+1} - Digite sua altura "
+  alturas << gets.to_f
+end
+
+puts "Idades: #{idades.reverse}"
+puts "Alturas: #{alturas.reverse}"
