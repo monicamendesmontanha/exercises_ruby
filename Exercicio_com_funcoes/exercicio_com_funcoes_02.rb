a6ba6c1c9aeb3416223d53d187848297f33a81d9
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-01 18:01:56 +1000

    Exercícios de funcoes adicionados

---
+++ Exercicio_com_funcoes/exercicio_com_funcoes_02.rb
@@ -0,0 +1,8 @@
+#Faça um programa para imprimir:
+#1 
+#1 2 
+#1 2 3 
+#.....
+#1 2 3 4 5 6 7 ...n
+#para um '''n''' informado pelo usuário. Use uma função que receba um
+#valor '''n''' inteiro imprima até a n-ésima linha.
