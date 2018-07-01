8a66e8e70d0beb6c7ae9715bd5421dc8428e73cb
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-01 17:43:26 +1000

    Exercício Incompleto (Temperatura e media anual)

--- Exercicio_com_listas/exercicio_com_listas_13.rb
+++ Exercicio_com_listas/exercicio_com_listas_13.rb
@@ -2,3 +2,21 @@
 #  armazene-as em uma lista. Após isto, calcule a média anual das temperaturas e
 #  mostre todas as temperaturas acima da média anual, e em que mês elas ocorreram
 #  (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
+
+todas_temperaturas = []
+
+12.times do |n|
+  print "Qual temperatura média do mês #{n + 1}? "
+  todas_temperaturas << gets.to_f
+end
+
+media_anual = todas_temperaturas.sum/todas_temperaturas.size
+
+acima_da_media = todas_temperaturas.select { |media| media > media_anual}
+puts "A medias acima de media anual observadas foram: #{acima_da_media}"
+
+#NAO SEI FAZER REFERENCIA DO "n" ao mês para que ele puxe automaticamente que n = 1 é igual a janeiro.
+#E DEPOIS DISSO NAO SEI ASSOCIAR O "n" de referência do mês relacionado a média daquele mês que por acaso vai tá sendo escolhida como acima da média.
+
+
+#puts "Os meses que ocorreram acima da media anual foram: #{}"
