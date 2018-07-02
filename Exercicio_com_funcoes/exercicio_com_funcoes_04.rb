d58a3bff7dc067c1684f8aba8eb6c70fa46bfed9
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-02 10:53:30 +1000

    Exercício Resolvido

--- Exercicio_com_funcoes/exercicio_com_funcoes_04.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_04.rb
@@ -1,3 +1,15 @@
 #Faça um programa, com uma função que necessite de um argumento. A função
 #retorna o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu
 #argumento for zero ou negativo.
+
+
+def retorna_valor(numero)
+  if numero >= 0
+    "positivo"
+  else numero < 0
+    "negativo"
+  end
+end
+
+resultado = retorna_valor(-8)
+puts resultado
