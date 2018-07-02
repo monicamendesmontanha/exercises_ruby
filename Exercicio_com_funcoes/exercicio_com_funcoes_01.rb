0035a6937a98483cde1d260b9035282fe0b7f478
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-02 10:34:35 +1000

    Exercício Incompleto (1,22,333,4444)

--- Exercicio_com_funcoes/exercicio_com_funcoes_01.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_01.rb
@@ -6,3 +6,12 @@
 #n n n n n n
 #para um '''n''' informado pelo usuário. Use uma função que receba um
 #valor '''n''' inteiro e imprima até a n-ésima linha.
+
+print "Digite até que número você quer que mostre? "
+numero = gets.to_i
+
+numero.times do |n|
+  puts "#{n + 1}"
+end
+
+#NAO SEI COMO IMPRIMIR CADA ELEMENTO A QUANTIDADE DE VEZES QUE SE REFERE A ELE MESMO, UM AO LADO DO OUTRO.
