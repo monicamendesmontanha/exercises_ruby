5542752808706591f3a4544a1f40fb8a221c5db6
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-02 10:46:27 +1000

    Exercício Resolvido (função simples)

--- Exercicio_com_funcoes/exercicio_com_funcoes_03.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_03.rb
@@ -1,2 +1,9 @@
 #Faça um programa, com uma função que necessite de três argumentos, e que
 #forneça a soma desses três argumentos.
+
+def soma(a, b, c)
+  a + b + c
+end
+
+resultado = soma(5,4,3)
+puts "#{resultado}"
