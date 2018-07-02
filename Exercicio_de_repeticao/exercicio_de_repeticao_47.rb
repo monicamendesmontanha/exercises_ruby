516c8be637addb5736fa1ed234b8ac9c9d489f9b
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-02 19:38:51 +1000

    Exercício Resolvido (Votos dos jurados)

--- Exercicio_de_repeticao/exercicio_de_repeticao_47.rb
+++ Exercicio_de_repeticao/exercicio_de_repeticao_47.rb
@@ -18,3 +18,28 @@
 #Melhor nota: 9.9
 #Pior nota: 7.5
 #Media: 9.04
+
+print "Competidor: "
+nome = gets.chomp
+
+TOTAL_DE_VOTOS = 7
+notas = []
+
+TOTAL_DE_VOTOS.times do |n|
+  print "#{n+1}ª Nota: "
+  notas << gets.to_f
+end
+
+notas_ordenadas = notas.sort
+melhor_nota = notas_ordenadas.last
+pior_nota = notas_ordenadas.first
+
+restante_das_notas = notas_ordenadas.select { |nota| nota != melhor_nota && nota != pior_nota}
+media_das_notas = restante_das_notas.sum / restante_das_notas.size
+
+puts "---------------"
+puts "Resultado Final"
+puts "Atleta: #{nome}"
+puts "Melhor nota: #{melhor_nota}"
+puts "Pior nota: #{pior_nota}"
+puts "Média: #{media_das_notas}"
