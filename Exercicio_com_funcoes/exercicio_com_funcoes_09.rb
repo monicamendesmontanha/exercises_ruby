6e16c6af6ce71648595b2a047f677458fcc77fa4
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-04 17:19:14 +1000

    Exercício Incompleto (Reveter a ordem do numero isolado fora do array)

--- Exercicio_com_funcoes/exercicio_com_funcoes_09.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_09.rb
@@ -1,2 +1,14 @@
 #''Reverso do número.''' Faça uma função que retorne o reverso de um número
 #  inteiro informado. Por exemplo: 127 -> 721.
+
+def reverte_numero(n)
+  n.reverse
+end
+
+numero = []
+
+print "Digite um numero: "
+numero << gets.to_i
+
+numero_revertido = reverte_numero(numero)
+puts "#{numero_revertido}"
