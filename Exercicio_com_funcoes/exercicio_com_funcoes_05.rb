8baa4231267d78892334a0df7e19a0fb2e1ddbbf
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-04 12:14:00 +1000

    Exercício Resolvido (imposto)

--- Exercicio_com_funcoes/exercicio_com_funcoes_05.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_05.rb
@@ -2,3 +2,15 @@
 #parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas
 #expressa em porcentagem e custo, que é o custo de um item antes do imposto. A
 #função “altera” o valor de custo para incluir o imposto sobre vendas.
+
+def SomaImposto(taxa_imposto, custo)
+  custo = (taxa_imposto * custo) + custo
+end
+
+print "Qual o valor do item? "
+valor = gets.to_f
+
+TAXA = 0.15
+
+altera = SomaImposto(TAXA, valor)
+puts "R$ #{altera}"
