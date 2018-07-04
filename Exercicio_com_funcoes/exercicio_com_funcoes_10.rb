c7fac9e74560403da4893dd844187faee073cd41
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-04 17:11:08 +1000

    Exercício Incompleto (Jogo de Craps)

--- Exercicio_com_funcoes/exercicio_com_funcoes_10.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_10.rb
@@ -5,3 +5,25 @@
 #você fez um 4, 5, 6, 8, 9 ou 10,este é seu "Ponto". Seu objetivo agora é continuar
 #jogando os dados até tirar este número novamente. Você perde, no entanto, se
 #tirar um 7 antes de tirar este Ponto novamente.
+
+
+print "{n1}º Dado lançado: "
+dado = gets.to_i
+
+if dado == 7 || dado == 11
+  puts "natural: você ganhou!"
+elsif dado == 2 || dado == 3 || dado == 12
+  puts "craps: você perdeu!"
+else
+  puts "ponto! continue jogando"
+end
+
+print "{n2}º Dado lançado: "
+segundo_dado = gets.to_i
+
+if segundo_dado != 7 
+  while segundo_dado != dado do
+    print "{ns}º Dado lançado: "
+    dado_seguintes = gets.to_i
+  end
+end
