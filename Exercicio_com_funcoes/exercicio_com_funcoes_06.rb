5481b00eed5145c3613c7de8602b27cdc4b1bedd
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-04 16:53:56 +1000

    Exercício Incompleto (Conversao da hora)

--- Exercicio_com_funcoes/exercicio_com_funcoes_06.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_06.rb
@@ -5,3 +5,43 @@
 #‘A’ para A.M. e ‘P’ para P.M. Assim, a função para efetuar as conversões terá um
 #parâmetro formal para registrar se é A.M. ou P.M. Inclua um loop que permita
 #que o usuário repita esse cálculo para novos valores de entrada todas as vezes que desejar.
+
+def converte_hora(hora)
+  equivalencia_da_hora = {
+    13 => "1",
+    14 => "2",
+    15 => "3",
+    16 => "4",
+    17 => "5",
+    18 => "6",
+    19 => "7",
+    20 => "8",
+    21 => "9",
+    22 => "10",
+    23 => "11",
+    24 => "12"
+  }
+end
+
+Hora = Struct.new(:h_agora, :h_convertida)
+
+print "Quer converter a hora da notação de 24 para 12 horas? [SIM/NAO]"
+resposta = gets.chomp
+
+if resposta == "SIM"
+  print "Digite que horas são: [24H]"
+  hora_agora = gets.to_i
+
+  print "Digite os minutos: "
+  minutos_agora = gets.to_i
+end
+
+hora = Hora.new(hora_agora, hora_convertida)
+
+if hora_agora > 12 
+  conversao = converte_hora(hora_agora)
+  hora_convertida = conversao
+  puts "#{hora_convertida}:#{minutos_agora}"
+else
+  puts "#{hora_agora}:#{minutos_agora}"
+end
