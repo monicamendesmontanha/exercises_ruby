43dda6aee96347782a05430f7cb1269486a04ff9
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-05 12:11:04 +1000

    Exercício Incompleto (Mesma lógica do exer10 porem com 3 vetores)

--- Exercicio_com_listas/exercicio_com_listas_11.rb
+++ Exercicio_com_listas/exercicio_com_listas_11.rb
@@ -1,1 +1,25 @@
 #Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
+
+
+QUANTIDADE_DE_ELEMENTOS = 4
+vetor_01 = []
+vetor_02 = []
+vetor_03 = []
+
+QUANTIDADE_DE_ELEMENTOS.times do |n|
+  puts "------------------"
+  puts "#{n+1}"
+  print "Digite o primeiro número: "
+  vetor_01 << gets.to_i
+  print "Digite o segundo número: "
+  vetor_02 << gets.to_i
+  print "Digite o terceiro número: "
+  vetor_03 << gets.to_i
+end
+
+
+puts "#{vetor_01}"
+puts "#{vetor_02}"
+puts "#{vetor_03}"
+
+#COMO PEGAR ELEMENTOS INTERCALADOS DOS TRÊS VETORES E CRIAR UM VETOR MISTO ?
