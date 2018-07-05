a0c08ae9300d4517278aea657e98a20e690690c2
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-05 12:05:50 +1000

    Exercício Incompleto (Qntidade dígitos de um nº qqr - fora de um array)

--- Exercicio_com_funcoes/exercicio_com_funcoes_08.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_08.rb
@@ -1,1 +1,13 @@
 #Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+
+def InformaQuantidadeDigitos (n)
+  n.size
+end
+
+numero = []
+
+print "Digite um número qualquer: "
+numero << gets.to_i
+
+quantidade_de_digitos_informados = InformaQuantidadeDigitos(numero)
+puts "O número possui #{quantidade_de_digitos_informados} dígitos."
