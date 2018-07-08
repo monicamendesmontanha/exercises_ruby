fa129b82f4241191ae66001ec119cb4df9a34ddf
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-08 19:36:33 +1000

    Exercício Resolvio (1, 22, 333..)

--- Exercicio_com_funcoes/exercicio_com_funcoes_01.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_01.rb
@@ -7,11 +7,20 @@
 #para um '''n''' informado pelo usuário. Use uma função que receba um
 #valor '''n''' inteiro e imprima até a n-ésima linha.
 
-print "Digite até que número você quer que mostre? "
-numero = gets.to_i
-
-numero.times do |n|
-  puts "#{n + 1}"
+def imprime_n_vezes(n)
+  n.times { |_| print "#{n} " }
 end
+
+def imprime_de_1_ate(n)
+  n.times do |numero_atual|
+    imprime_n_vezes(numero_atual + 1)
+    puts ""
+  end
+end
+
+print "Digite até que número você quer que mostre? "
+n = gets.to_i
+
+imprime_de_1_ate(n)
 
 #NAO SEI COMO IMPRIMIR CADA ELEMENTO A QUANTIDADE DE VEZES QUE SE REFERE A ELE MESMO, UM AO LADO DO OUTRO.
