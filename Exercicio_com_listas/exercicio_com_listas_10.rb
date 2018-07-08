766781b152c9ffe1b019b9f18db8e5e08bcd3fc3
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-08 19:51:00 +1000

    Exercício Resolvido (intercala 2 vetores)

--- Exercicio_com_listas/exercicio_com_listas_10.rb
+++ Exercicio_com_listas/exercicio_com_listas_10.rb
@@ -1,22 +1,40 @@
-#Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos, 
+#Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos,
 #cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
 
+def cria_vetor(quantidade_elementos)
+  vetor = []
 
-QUANTIDADE_DE_ELEMENTOS = 4
-vetor_01 = []
-vetor_02 = []
+  quantidade_elementos.times do |n|
+    puts "------------------"
+    puts "#{n+1}"
+    print "Digite o número: "
+    vetor << gets.to_i
+  end
 
-QUANTIDADE_DE_ELEMENTOS.times do |n|
-  puts "------------------"
-  puts "#{n+1}"
-  print "Digite o primeiro número: "
-  vetor_01 << gets.to_i
-  print "Digite o segundo número: "
-  vetor_02 << gets.to_i
+  vetor
 end
 
+def intercala_vetor(quantidade_elementos, a, b)
+  vetor = []
+
+  quantidade_elementos.times do |index|
+    vetor << a[index]
+    vetor << b[index]
+  end
+
+  vetor
+end
+
+QUANTIDADE_DE_ELEMENTOS = 4
+
+puts "vetor 01:"
+vetor_01 = cria_vetor(QUANTIDADE_DE_ELEMENTOS)
+
+puts "vetor 02:"
+vetor_02 = cria_vetor(QUANTIDADE_DE_ELEMENTOS)
 
 puts "#{vetor_01}"
 puts "#{vetor_02}"
 
-#COMO PEGAR ELEMENTOS INTERCALADOS DOS DOIS VETORES E CRIAR UM VETOR MISTO ?
+vetor_intercalado = intercala_vetor(QUANTIDADE_DE_ELEMENTOS, vetor_01, vetor_02)
+puts "#{vetor_intercalado}"
