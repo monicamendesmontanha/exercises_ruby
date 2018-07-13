cae3a312ffb942c614b296548b11d0c30da4f50c
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-13 16:01:39 +1000

    Exercício Incompleto - "B"( votação de jogadores)

--- Exercicio_com_listas/exercicio_com_listas_18.rb
+++ Exercicio_com_listas/exercicio_com_listas_18.rb
@@ -1,28 +1,28 @@
-#Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para 
-#saber qual o melhor jogador após cada jogo. Para isto, faz-se necessário o desenvolvimento 
-#de um programa, que será utilizado pelas telefonistas, para a computação dos votos. 
-#Sua equipe foi contratada para desenvolver este programa, utilizando a linguagem de 
+#Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para
+#saber qual o melhor jogador após cada jogo. Para isto, faz-se necessário o desenvolvimento
+#de um programa, que será utilizado pelas telefonistas, para a computação dos votos.
+#Sua equipe foi contratada para desenvolver este programa, utilizando a linguagem de
 #programação C++. Para computar cada voto, a telefonista digitará um número, entre 1 e 23,
-#correspondente ao número da camisa do jogador. Um número de jogador igual zero, indica que 
-#a votação foi encerrada. Se um número inválido for digitado, o programa deve ignorá-lo, 
-#mostrando uma breve mensagem de aviso, e voltando a pedir outro número. 
+#correspondente ao número da camisa do jogador. Um número de jogador igual zero, indica que
+#a votação foi encerrada. Se um número inválido for digitado, o programa deve ignorá-lo,
+#mostrando uma breve mensagem de aviso, e voltando a pedir outro número.
 
 #Após o final da votação, o programa deverá exibir:
 #a. O total de votos computados;
 #b. Os númeos e respectivos votos de todos os jogadores que receberam votos;
 #c. O percentual de votos de cada um destes jogadores;
-#d. O número do jogador escolhido como o melhor jogador da partida, juntamente com o número 
+#d. O número do jogador escolhido como o melhor jogador da partida, juntamente com o número
 #de votos e o percentual de votos dados a ele.
 
-#Observe que os votos inválidos e o zero final não devem ser computados como votos. 
+#Observe que os votos inválidos e o zero final não devem ser computados como votos.
 #O resultado aparece ordenado pelo número do jogador. O programa deve fazer uso de arrays.
-#O programa deverá executar o cálculo do percentual de cada jogador através de uma função. 
-#Esta função receberá dois parâmetros: o número de votos de um jogador e o total de votos. 
+#O programa deverá executar o cálculo do percentual de cada jogador através de uma função.
+#Esta função receberá dois parâmetros: o número de votos de um jogador e o total de votos.
 #A função calculará o percentual e retornará o valor calculado.
-#Abaixo segue uma tela de exemplo. O disposição das informações deve ser o mais próxima 
+#Abaixo segue uma tela de exemplo. O disposição das informações deve ser o mais próxima
 #possível ao exemplo. Os dados são fictícios e podem mudar a cada execução do programa.
 
-#Ao final, o programa deve ainda gravar os dados referentes ao resultado da votação em um arquivo 
+#Ao final, o programa deve ainda gravar os dados referentes ao resultado da votação em um arquivo
 #texto no disco, obedecendo a mesma disposição apresentada na tela.
 
 #Enquete: Quem foi o melhor jogador?
@@ -46,38 +46,111 @@
 #10            3%            37,5%
 #11            1%            12,5%
 
+def total_de_votos_computados(votacao)
+  votacao.size
+end
+
+def numeros_e_respectivos_votos_de_todos_os_jogadores_que_receberam_votos(jogadores)
+  votacao_customizada = {}
+
+  jogadores.each do |numero, votos|
+    if votos.size > 0
+      votacao_customizada[numero] = votos.size
+    end
+  end
+
+  votacao_customizada
+end
+
+def percentual_de_votos_de_cada_um_destes_jogadores(votacao)
+  puts "PENDENTE"
+end
+
+def numero_do_jogador_escolhido_como_o_melhor_jogador_da_partida_juntamente_com_o_numero_de_votos_e_o_percentual_de_votos_dados_a_ele(votacao)
+  puts "PENDENTE"
+end
+
+jogadores = {
+  1 => [],
+  2 => [],
+  3 => [],
+  4 => [],
+  5 => [],
+  6 => [],
+  7 => [],
+  8 => [],
+  9 => [],
+  10 => [],
+  11 => [],
+  12 => [],
+  13 => [],
+  14 => [],
+  15 => [],
+  16 => [],
+  16 => [],
+  17 => [],
+  18 => [],
+  19 => [],
+  20 => [],
+  21 => [],
+  22 => [],
+  23 => []
+}
+
 #O melhor jogador foi o número 9, com 4 votos, correspondendo a 50% dos votos.
-def vota_no_jogador 
+def vota_no_jogador
   puts "--------------VOTAÇÃO--------------"
   puts "[Informe um valor entre 1 e 23]"
   print "Qual nº de jogador você vai votar? "
-  numero = gets.to_i
+
+  voto = gets.to_i
+
+  while votacao_invalida?(voto) do
+    puts "----------------------------------"
+    puts "Não existe jogador com este número."
+    puts "Por favor, digite um número válido."
+    puts "----------------------------------"
+
+    voto = vota_no_jogador
+  end
+
+  voto
 end
 
 def votacao_invalida? (numero)
-  numero > 24
+  numero < 0 || numero > 23
 end
 
-def encerrar_votacao(numero)
-  numero == 0
+def votacao_em_andamento?(numero)
+  numero != 0
 end
 
-#O porque do programa estar dando LOOP INFINITO no momento que digito ZERO?
+votacao = []
+numero_votado = vota_no_jogador
+votacao << numero_votado
+jogadores[numero_votado] << 1
 
-votacao = vota_no_jogador
+while votacao_em_andamento?(numero_votado) do
+  numero_votado = vota_no_jogador
 
-while votacao_invalida?(votacao) do
-  puts "----------------------------------"
-  puts "Não existe jogador com este número." 
-  puts "Por favor, digite um número válido."
-  puts "----------------------------------"
-
-  votacao = vota_no_jogador 
+  if numero_votado != 0
+    votacao << numero_votado
+    jogadores[numero_votado] << 1
+  end
 end
 
-while encerrar_votacao(votacao)
-  puts "Votação encerrada!"
-end
+votacao_encerrada = votacao.reject { |n| n == 0 }
 
 puts "Votação realizada com sucesso!"
 
+puts "a. #{total_de_votos_computados(votacao_encerrada)}"
+
+resposta_b = numeros_e_respectivos_votos_de_todos_os_jogadores_que_receberam_votos(jogadores)
+
+puts "b."
+resposta_b.each do |numero, total_votos|
+  puts "O jogador de número #{numero} recebeu #{total_votos}."
+end
+
+puts "c. #{percentual_de_votos_de_cada_um_destes_jogadores(votacao_encerrada)}"
+puts "d. #{numero_do_jogador_escolhido_como_o_melhor_jogador_da_partida_juntamente_com_o_numero_de_votos_e_o_percentual_de_votos_dados_a_ele(votacao_encerrada)}"
