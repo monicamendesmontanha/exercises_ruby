40b91e07ccf2decbb7f9664380fa86c813a81152
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-14 19:03:28 +1000

    exercicio adicionado

---
+++ Exercicio_com_funcoes/exercicio_com_funcao_15.rb
@@ -0,0 +1,4 @@
+#Elabore uma função que identifica e mostra na tela todos os quadrados mágicos com as 
+#características acima. Dica: produza todas as combinações possíveis e verifique a soma 
+#quando completar cada quadrado. Usar um vetor de 1 a 9 parece ser mais simples que usar 
+#uma matriz 3x3.
