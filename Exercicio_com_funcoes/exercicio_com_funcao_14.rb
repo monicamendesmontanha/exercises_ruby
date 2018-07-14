0c16540ba9d161fed9021e53b8617ff880658dda
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-14 19:02:18 +1000

    exercicio adicionado

---
+++ Exercicio_com_funcoes/exercicio_com_funcao_14.rb
@@ -0,0 +1,7 @@
+#'''Quadrado mágico'''. Um quadrado mágico é aquele dividido em linhas e colunas,
+#com um número em cada posição e no qual a soma das linhas, colunas e diagonais
+#é a mesma. Por exemplo, veja um quadrado mágico de lado 3, com números de 1 a 9:
+
+#8 3 4
+#1 5 9
+#6 7 2
