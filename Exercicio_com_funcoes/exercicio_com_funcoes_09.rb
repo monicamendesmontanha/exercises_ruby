8a6c71ef8d77082bd8c942c85daea5562903691a
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-16 20:26:08 +1000

    Exercício Resolvido (numero revertido)

--- Exercicio_com_funcoes/exercicio_com_funcoes_09.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_09.rb
@@ -1,14 +1,8 @@
 #''Reverso do número.''' Faça uma função que retorne o reverso de um número
 #  inteiro informado. Por exemplo: 127 -> 721.
 
-def reverte_numero(n)
-  n.reverse
-end
-
-numero = []
-
 print "Digite um numero: "
-numero << gets.to_i
+numero = gets.chomp
+numero_revertido = numero.reverse
 
-numero_revertido = reverte_numero(numero)
 puts "#{numero_revertido}"
