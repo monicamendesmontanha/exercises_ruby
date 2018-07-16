74733e309598b0a429107d714d7b072f850a99c8
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-16 16:54:26 +1000

    atualização exercicio

--- Exercicio_com_strings/exercicio_com_strings_11.rb
+++ Exercicio_com_strings/exercicio_com_strings_11.rb
@@ -1,3 +1,12 @@
 #'''Jogo de Forca.''' Desenvolva um jogo da forca. O programa terá uma lista de
 #palavras lidas de um arquivo texto e escolherá uma aleatoriamente. O jogador
 #poderá errar 6 vezes antes de ser enforcado.
+
+#Digite uma letra: A
+#-> Você errou pela 1a vez. Tente de novo!
+#Digite uma letra: O
+#A palavra é: _ _ _ _ O
+#Digite uma letra: E
+#A palavra é: _ E _ _ O
+#Digite uma letra: S
+#-> Você errou pela 2a vez. Tente de novo!
