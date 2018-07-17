a81432b0d31a193c298613efee5562b8a70c86d7
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-17 11:56:28 +1000

    exercício incompleto (conta vogais e espaços)

--- Exercicio_com_strings/exercicio_com_strings_07.rb
+++ Exercicio_com_strings/exercicio_com_strings_07.rb
@@ -2,3 +2,19 @@
 #usuário (incluindo espaços em branco), conte:
 #a. quantos espaços em branco existem na frase.
 #b. quantas vezes aparecem as vogais a, e, i, o, u.
+vogais = {
+  "a" => [],
+  "b" => [],
+  "c" => [],
+  "d" => [],
+  "e" => []
+}
+
+
+print "Digite uma frase: "
+frase = gets.split('')
+
+puts frase
+
+  frase.each do |n| 
+  end
