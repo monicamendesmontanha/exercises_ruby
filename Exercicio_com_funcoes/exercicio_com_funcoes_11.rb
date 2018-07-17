e97c07ed16d082a3963f9b03b41f58379b51c39b
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-17 11:38:06 +1000

    Exercício Incompleto (validar data)

--- Exercicio_com_funcoes/exercicio_com_funcoes_11.rb
+++ Exercicio_com_funcoes/exercicio_com_funcoes_11.rb
@@ -1,3 +1,33 @@
-#'''Data com mês por extenso'''. Construa uma função que receba uma data noformato ''DD/MM/AAAA'' e devolva uma string no formato ''D de
-# mesPorExtenso de AAAA''. Opcionalmente, valide a data e retorne NULL caso a
-# data seja inválida.
+#'''Data com mês por extenso'''. Construa uma função que receba uma data noformato ''DD/MM/AAAA'' 
+#e devolva uma string no formato ''D de mesPorExtenso de AAAA''. Opcionalmente, valide a data 
+#e retorne NULL caso a data seja inválida.
+
+
+def dia_valido(dia)
+  dia > 1 or dia <= 31
+end
+
+
+meses = {
+  1 => "Janeiro",
+  2 => "Fevereiro",
+  3 => "Março",
+  4 => "Abril",
+  5 => "Maio",
+  6 => "Junho",
+  7 => "Julho",
+  8 => "Agosto",
+  9 => "Setemebro",
+  10 => "Outubro",
+  11 => "Novembro",
+  12 => "Dezembro"
+}
+
+print "Digite a data: [dd/mm/aaaa] "
+data = gets.chomp.split('/')
+
+dia = dia_valido(data[0])
+mes = data[1].to_i
+ano = data[2]
+
+puts "#{dia} de #{meses[mes]} de #{ano}"
