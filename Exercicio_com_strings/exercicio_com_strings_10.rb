6f430146bd2258d0fc4779d9e2fa5dba0b97f760
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-18 11:31:21 +1000

    Exercício Incompleto (numero por extenso ate 99)

--- Exercicio_com_strings/exercicio_com_strings_10.rb
+++ Exercicio_com_strings/exercicio_com_strings_10.rb
@@ -1,2 +1,37 @@
 #'''Número por extenso.''' Escreva um programa que solicite ao usuário a digitação
 #de um número até 99 e imprima-o na tela por extenso.
+
+numeros = {
+  1 => "um",
+  2 => "dois",
+  3 => "três",
+  4 => "quatro",
+  5 => "cinco",
+  6 => "seis",
+  7 => "sete",
+  8 => "oito",
+  9 => "nove",
+
+  11 => "onze",
+  12 => "doze",
+  13 => "treze",
+  14 => "quartorze",
+  15 => "quinze",
+  16 => "dezesseis",
+  17 => "dezessete",
+  18 => "dezoito",
+  19 => "dezenove",
+
+
+  10 => "dez",  
+  20 => "vinte",
+  30 => "trinta"
+}
+
+
+
+print "Digite um numero que você quer saber por extenso: "
+n = gets.to_i
+
+puts "Número: #{n}"
+puts "Número por extenso: #{numeros[n]}"
