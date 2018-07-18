78b60dfbfb0fa69a23333e59d174b5dc44648fb2
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-18 11:20:31 +1000

    Exercício Resolvido (Frase palíndroma)

--- Exercicio_com_strings/exercicio_com_strings_08.rb
+++ Exercicio_com_strings/exercicio_com_strings_08.rb
@@ -4,3 +4,15 @@
 #ignorados. A frase '''SUBI NO ONIBUS''' é o exemplo de uma frase palíndroma
 #onde os espaços foram ignorados. Faça um programa que leia uma seqüência de
 #caracteres, mostre−a e diga se é um palíndromo ou não.
+
+print "Digite uma frase: "
+frase = gets.chomp.sub(" ", "") 
+
+frase_revertida = frase.reverse
+puts frase_revertida.sub(" ", "") 
+
+if frase == frase_revertida
+  puts "Frase palíndroma"
+else
+  puts "NÃO é frase palíndroma"
+end
