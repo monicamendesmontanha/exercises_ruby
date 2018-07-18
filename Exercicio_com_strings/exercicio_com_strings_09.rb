ae2a4b19a59f0cdae08358b34f3bfd186f807b72
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-18 10:20:54 +1000

    Exercício Resolvido (cpf = gets.chomp.gsub(/\.|-/, ''))

--- Exercicio_com_strings/exercicio_com_strings_09.rb
+++ Exercicio_com_strings/exercicio_com_strings_09.rb
@@ -1,3 +1,11 @@
 #'''Verificação de CPF.''' Desenvolva um programa que solicite a digitação de um
 #número de CPF no formato '''xxx.xxx.xxx-xx''' e indique se é um número válido
 #ou inválido através da validação dos dígitos verificadores edos caracteres de formatação.
+
+def cpf_valido?(cpf)
+end
+
+print "Digite seu CPF: [xxx.xxx.xxx-xx]"
+cpf = gets.chomp.gsub(/\.|-/, '')
+
+puts cpf
