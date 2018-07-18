43ebfd94e8e8f997303fc51fa542111fbd869923
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-18 15:06:45 +1000

    Exercício Resolvido (dado lançado)

--- Exercicio_com_listas/exercicio_com_listas_24.rb
+++ Exercicio_com_listas/exercicio_com_listas_24.rb
@@ -2,3 +2,27 @@
 #e armazene os resultados em um vetor . Depois, mostre quantas vezes cada valor
 #foi conseguido. Dica: use um vetor de contadores(1-6) e uma função para gerar
 #numeros aleatórios, simulando os lançamentos dos dados.
+
+numeros_do_dado = {
+  1 => [],
+  2 => [],
+  3 => [],
+  4 => [],
+  5 => [],
+  6 => []
+}
+
+10.times do
+  print "Lance o dado: "
+  resultado = gets.to_i
+
+  numeros_do_dado[resultado] << 1
+end
+
+numeros_do_dado.each do |numero, resultado|
+  if resultado.size > 0
+    numeros_do_dado[numero] = resultado.size
+  end
+end
+
+puts numeros_do_dado
