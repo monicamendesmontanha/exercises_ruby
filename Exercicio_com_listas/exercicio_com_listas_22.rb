6af2f1ccd1ee121e47b5bca244539b9e4c36aada
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-07-31 10:15:47 +1000

    Início da resolução

--- Exercicio_com_listas/exercicio_com_listas_22.rb
+++ Exercicio_com_listas/exercicio_com_listas_22.rb
@@ -6,10 +6,10 @@
 #O programa deverá receber um número indeterminado de entradas, cada uma contendo:
 #um número de identificação do mouse o tipo de defeito:
 
-#  a. necessita da esfera;
+#a. necessita da esfera;
 #b. necessita de limpeza;
-#c.necessita troca do cabo ou conector;
-#d.quebrado ou inutilizado
+#c. necessita troca do cabo ou conector;
+#d. quebrado ou inutilizado
 
 #Uma identificação igual a zero encerra o programa. 
 #Ao final o programa deverá emitir o seguinte relatório:
@@ -21,3 +21,69 @@
 #2- necessita de limpeza                                  30                   30%
 #3- necessita troca do cabo ou conector                   15                   15%
 #4- quebrado ou inutilizado                               15                   15%
+
+def solicita_tipo_de_defeito
+
+  puts "-------------------------------------------"
+  puts "TIPOS DE DEFEITOS DO MOUSE"
+  puts "a. necessita da esfera;"
+  puts "b. necessita de limpeza;"
+  puts "c. necessita troca do cabo ou conector;"
+  puts "d. quebrado ou inutilizado"
+  print " => Identifique o tipo de defeito encontrado no mouse: "
+  tipo = gets
+
+end 
+
+  # "a": {
+  #   situacao: "necessita da esfera",
+  #   quantidade: [],
+  #   percentual: [] 
+  # },
+  # "b": {
+  #   situacao: "necessita de limpeza",
+  #   quantidade: [],
+  #   percentual: [] 
+  # },
+  # "c": {
+  #   situacao: "necessita troca de cabo ou conector",
+  #   quantidade: [],
+  #   percentual: [] 
+  # },
+  # "d": {
+  #   situacao: "quebrado ou inutilizado",
+  #   quantidade: [],
+  #   percentual: [] 
+  # }
+
+identificacao = []
+
+class Identificadores
+  
+  attr_reader :situacao, :quantidade, :percentual
+
+  def initialize(situacao, quantidade, percentual)
+    @situacao = situacao
+    @quantidade = quantidade
+    @percentual = percentual
+  end
+
+end
+
+
+5.times do
+  tipo_de_defeito = solicita_tipo_de_defeito
+  
+  identificador_a = Identificadores.new("necessita da esfera", [:quantidade], [:percentual])
+  identificador_b = Identificadores.new("necessita de limpeza", [:quantidade], [:percentual])
+  
+  identificacao << Identificadores
+
+  identificacao.each do |tipo_de_defeito, valor|
+    valor[@quantidade] << 1 
+  end
+
+  puts identificacao
+
+end
+
