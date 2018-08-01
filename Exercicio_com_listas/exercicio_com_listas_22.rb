d8c0df5c1e18d047c99b7bb6582a913899176c83
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-08-01 12:13:12 +1000

    opçao de como chamar um novo objeto dentro do array

--- Exercicio_com_listas/exercicio_com_listas_22.rb
+++ Exercicio_com_listas/exercicio_com_listas_22.rb
@@ -47,25 +47,24 @@ def solicita_tipo_de_defeito
 
 end 
 
-identificador_a = Identificadores.new('a', "necessita da esfera", 0)
-identificador_b = Identificadores.new('b', "necessita de limpeza", 0)
-identificador_c = Identificadores.new('c', "necessita troca de cabo ou conector", 0)
-identificador_d = Identificadores.new('d', "quebrado ou inutilizado", 0)
+# identificador_a = Identificadores.new('a', "necessita da esfera", 0)
+# identificador_b = Identificadores.new('b', "necessita de limpeza", 0)
+# identificador_c = Identificadores.new('c', "necessita troca de cabo ou conector", 0)
+# identificador_d = Identificadores.new('d', "quebrado ou inutilizado", 0)
 
-identificacao = [
-  identificador_a, 
-  identificador_b,
-  identificador_c,
-  identificador_d
-]
-
-#  é certo? como minimizar esse código acima?
 # identificacao = [
-#   identificador_a = Identificadores.new('a', "necessita da esfera", 0)
-#   identificador_b = Identificadores.new('b', "necessita de limpeza", 0)
-#   identificador_c = Identificadores.new('c', "necessita troca de cabo ou conector", 0)
-#   identificador_d = Identificadores.new('d', "quebrado ou inutilizado", 0)
+#   identificador_a, 
+#   identificador_b,
+#   identificador_c,
+#   identificador_d
 # ]
+
+ identificacao = [
+  Identificadores.new('a', "necessita da esfera", 0),
+  Identificadores.new('b', "necessita de limpeza", 0),
+  Identificadores.new('c', "necessita troca de cabo ou conector", 0),
+  Identificadores.new('d', "quebrado ou inutilizado", 0)
+ ]
 
 
 10.times do
